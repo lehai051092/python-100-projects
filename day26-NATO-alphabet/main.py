d95cdf25401375c51263b7053d23d412e31550a0
HaiLT <bibo051092@gmail.com>
2024-12-09 22:56:06 +0700

    day30-errors-exceptions-and-json-data

--- day26-NATO-alphabet/main.py
+++ day26-NATO-alphabet/main.py
@@ -3,7 +3,16 @@ import pandas
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 dictionary_nato_alphabet = {item.letter:item.code for index, item in nato_alphabet.iterrows()}
 
-text = input("input your text: ").upper()
-result = [dictionary_nato_alphabet[char] for char in list(text)]
 
-print(f"result {result}")
+def generate_phonetic():
+    text = input("input your text: ").upper()
+
+    try:
+        result = [dictionary_nato_alphabet[char] for char in list(text)]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(f"result {result}")
+
+generate_phonetic()
